43ffeeb8fafc5498af3d3a451331cc7f6bd796a0
Tatiana Manakhova <tat.manakhova@gmail.com>
2021-02-11 19:40:03 +0300

    Настраивает маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import Main from '../main';
+import SignInPage from '../sign-in-page';
+import FavoritesPage from '../favorites';
+import OfferPage from '../offer';
+import NotFoundPage from '../not-found-page';
 
 const App = (props) => {
   const {citiesCount} = props;
 
   return (
-    <Main citiesCount={citiesCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main citiesCount={citiesCount} />
+        </Route>
+        <Route exact path="/login">
+          <SignInPage />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesPage />
+        </Route>
+        <Route exact path="/offer/:id">
+          <OfferPage />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
