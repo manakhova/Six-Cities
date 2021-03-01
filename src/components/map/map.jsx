51227b66cea1dda4ba8284258fe247e017f2ce72
Tatiana Manakhova <tat.manakhova@gmail.com>
2021-03-01 23:18:48 +0300

    Добалвяет leaflet

---
+++ src/components/map/map.jsx
@@ -0,0 +1,70 @@
+import React, {useEffect, useRef} from 'react';
+import leaflet from 'leaflet';
+import PropTypes from 'prop-types';
+
+import "leaflet/dist/leaflet.css";
+
+const Map = ({city, offers}) => {
+  const mapRef = useRef();
+
+  useEffect(() => {
+    mapRef.current = leaflet.map(`map`, {
+      center: {
+        lat: city.lat,
+        lng: city.lng
+      },
+      zoom: city.zoom,
+      zoomControl: false,
+      marker: true
+    });
+    mapRef.current.setView([city.lat, city.lng], city.zoom);
+
+    leaflet
+      .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+        attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+      })
+      .addTo(mapRef.current);
+
+    offers.forEach((offer) => {
+      const customIcon = leaflet.icon({
+        iconUrl: `img/pin.svg`,
+        iconSize: [30, 30]
+      });
+
+      leaflet.marker({
+        lat: offer.location.latitude,
+        lng: offer.location.longitude
+      },
+      {
+        icon: customIcon
+      })
+      .addTo(mapRef.current)
+      .bindPopup(offer.title);
+
+      return () => {
+        mapRef.current.remove();
+      };
+    });
+  }, []);
+
+  return (
+    <section className="cities__map map" id="map" style={{height: `100%`}} ref={mapRef}></section>
+  );
+};
+
+Map.propTypes = {
+  city: PropTypes.shape({
+    lat: PropTypes.number.isRequired,
+    lng: PropTypes.number.isRequired,
+    zoom: PropTypes.number.isRequired,
+  }),
+  offers: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    location: PropTypes.shape({
+      latitude: PropTypes.number.isRequired,
+      longitude: PropTypes.number.isRequired,
+    })
+  }))
+};
+
+export default Map;
--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -4,8 +4,8 @@ export default [{
   bedrooms: 3,
   city: {
     location: {
-      latitude: 52.370216,
-      longitude: 4.895168,
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
       zoom: 10
     },
     name: `Amsterdam`
@@ -23,8 +23,8 @@ export default [{
   isFavorite: true,
   isPremium: false,
   location: {
-    latitude: 52.35514938496378,
-    longitude: 4.673877537499948,
+    latitude: 52.3909553943508,
+    longitude: 4.85309666406198,
     zoom: 8
   },
   maxAdults: 4,
@@ -37,8 +37,8 @@ export default [{
   bedrooms: 2,
   city: {
     location: {
-      latitude: 52.370216,
-      longitude: 4.895168,
+      latitude: 52.369553943508,
+      longitude: 4.85309666406198,
       zoom: 10
     },
     name: `Amsterdam`
@@ -56,8 +56,8 @@ export default [{
   isFavorite: true,
   isPremium: true,
   location: {
-    latitude: 52.35514938496378,
-    longitude: 4.673877537499948,
+    latitude: 52.369553943508,
+    longitude: 4.85309666406198,
     zoom: 8
   },
   maxAdults: 2,
@@ -70,8 +70,8 @@ export default [{
   bedrooms: 4,
   city: {
     location: {
-      latitude: 52.370216,
-      longitude: 4.895168,
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
       zoom: 10
     },
     name: `Amsterdam`
@@ -89,8 +89,8 @@ export default [{
   isFavorite: false,
   isPremium: true,
   location: {
-    latitude: 52.35514938496378,
-    longitude: 4.673877537499948,
+    latitude: 52.3909553943508,
+    longitude: 4.929309666406198,
     zoom: 8
   },
   maxAdults: 6,
@@ -103,8 +103,8 @@ export default [{
   bedrooms: 3,
   city: {
     location: {
-      latitude: 52.370216,
-      longitude: 4.895168,
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
       zoom: 10
     },
     name: `Amsterdam`
@@ -122,8 +122,8 @@ export default [{
   isFavorite: false,
   isPremium: false,
   location: {
-    latitude: 52.35514938496378,
-    longitude: 4.673877537499948,
+    latitude: 52.3809553943508,
+    longitude: 4.939309666406198,
     zoom: 8
   },
   maxAdults: 4,
